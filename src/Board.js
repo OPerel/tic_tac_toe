2df9b383938f5cc96762188a097d944d3bad6610
Operel <oriperelman@gmail.com>
2019-05-07 11:53:58 +0300

    changed Board to class

--- src/Board.js
+++ src/Board.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Board.css';
 
-const Board = (props) => {
-    return (
-        <div id="b" className='board'>   
-            {props.children}
-        </div>
-    )
-}
+class Board extends Component {
+    render () {
+        return (
+            <div id="b" className='board'>
+                {this.props.children}
+            </div>
+        );
+    };
+};
 
 export default Board;
