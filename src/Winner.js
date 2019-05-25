352065a3ff525cda1295cfd0dcfb9ae751e513b6
Operel <oriperelman@gmail.com>
2019-05-25 14:20:02 +0300

    minor refactoring

--- src/Winner.js
+++ src/Winner.js
@@ -5,7 +5,10 @@ import Button from './Button';
 const Winner = ({ winner, click, buttonText }) => {
     return (
         <div id="w" className={winner ? "winner win" : "winner"}>
-            <h2><span>{winner !== 'tie' ? winner : ''}</span>{winner === 'X' || winner === 'Y' ? ' Player wins!' : 'Tie! No player wins.'}</h2>
+            <h2>
+                <span>{winner !== 'tie' ? winner : ''}</span>
+                {winner === 'X' || winner === 'Y' ? ' Player wins!' : 'Tie! No player wins.'}
+            </h2>
             <Button click={click} text={buttonText} />
         </div>
     );
